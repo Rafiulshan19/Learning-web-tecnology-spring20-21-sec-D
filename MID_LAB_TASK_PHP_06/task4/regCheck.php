813c20615409f243132d3619e844b993a5a0dde7
Rafiulshan19 <18-38321-2@student.aiub.edu>
2021-03-01 00:44:04 +0600

    Registration

---
+++ MID_LAB_TASK_PHP_06/task4/regCheck.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+
+	if(isset($_POST['submit'])){
+		$name 		= $_POST['name'];
+		$email 		= $_POST['email'];
+		$username 	= $_POST['username'];
+		$password 	= $_POST['password'];
+		$repass 	= $_POST['repass'];
+		$gender 	= $_POST['gender'];
+		$day		= $_POST['day'];
+		$month		= $_POST['month'];
+		$year		= $_POST['year'];
+		
+		if($username == "" || $name == "" || $email == "" || $password == ""|| $repass == ""|| $gender == ""|| $day == ""|| $month == ""|| $year == ""){
+			echo "invalid information...please try again!";
+		}else{
+
+			if($password == $repass){
+
+				echo "REGISTRATION DONE ";
+			}else{
+				echo "password & re-type password mismatch...";
+			}
+		}
+	}
+?>
